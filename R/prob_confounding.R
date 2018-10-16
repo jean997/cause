3b5d97e28ce640c06c822bc1d809af2c3ee48d3d
Jean Morrison <jeanm@uchicago.edu>
2018-10-16 12:46:06 -0500

    prob_confounding added

---
+++ R/prob_confounding.R
@@ -0,0 +1,40 @@
+
+#'@title Posterior probability of association with confounder
+#'@description For a set of data, X, calculate the probability that each variant
+#'acts through the confounder
+#'@param X object of class cause_data. This need not be the same data used to compute 'fit'.
+#'@param fit object of class cause_post.
+#'@param nsamps Number of samples to take from the posterior (see Details).
+#'@details
+#'Let x_i represent the the data in the ith row of 'X'. Let Z_i be the indicator that
+#'variant i acts through the confounder. We are interested in computing P(Z_i = 1 | posteriors) where
+#'posteriors refers to the posterior parameter distributions contained in  'fit'.
+#' We compute l(x_i | posteriors, Z=1) and l(x_i | posteriors, Z_i = 0) where l denotes the likelihood.
+#' We then comput P(Z_i = 1 | posteriors) = l(x_i | posteriors, Z_i = 1)/(l(x_i | posteriors, Z_i = 1) + l(x_i | posteriors, Z_i = 0))
+#' @return an object of class cause_data and data.frame. The returned object is identical to X
+#' but contains additional columns ll_Z1, ll_Z0, and prob_Z1.
+#'@export
+prob_confounding <- function(X, fit, nsamps=1000){
+  if(!inherits(X, "cause_data")){
+    warning("Converting X to cause_data.")
+    X <- new_cause_data(X)
+  }
+  stopifnot(inherits(fit, "cause_post"))
+  samps <-samp_from_grid(fit$joint_post,
+                         c("q", "gamma", "eta"), nsamps)
+  mix_grid <- fit$mix_grid
+  rho <- fit$rho
+  ll_mat_Z1 <- loglik_samps_Z1(samps$gamma, samps$eta, samps$q, rho,
+                      mix_grid$S1, mix_grid$S2, mix_grid$pi,
+                      X$beta_hat_1, X$beta_hat_2,
+                      X$seb1, X$seb2)
+
+  ll_mat_Z0 <- loglik_samps_Z0(samps$gamma, samps$eta, samps$q, rho,
+                              mix_grid$S1, mix_grid$S2, mix_grid$pi,
+                              X$beta_hat_1, X$beta_hat_2,
+                              X$seb1, X$seb2)
+  X$ll_Z1 = rowMeans(ll_mat_Z1)
+  X$ll_Z0 = rowMeans(ll_mat_Z0)
+  X$prob_Z1 <- with(X, exp(ll_Z1)/(exp(ll_Z1) + exp(ll_Z0)))
+  return(X)
+}
