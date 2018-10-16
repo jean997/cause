6309cc10890aad3e6685d7d0c0dda267f336b310
Jean Morrison <jeanm@uchicago.edu>
2018-10-16 12:47:11 -0500

    updated samp_from_grid to new arg names

--- R/samp_from_grid.R
+++ R/samp_from_grid.R
@@ -2,26 +2,26 @@
 #'@description Sample from the joint posterior of parameters
 #'@param joint_post Data frame giving the joint posterior
 #'@param params Vector of names of parameters to sample
-#'@params nsamps Number of samples to take
-#'@retunr A data.frame that is nsamps by length(params) of samples
+#'@param nsamps Number of samples to take
+#'@return A data.frame that is nsamps by length(params) of samples
 #'@export
-samp_from_grid <- function(post, params, nsamps){
+samp_from_grid <- function(joint_post, params, nsamps){
   #will look for variable names x, xstart, xstop where x is a parameter
   #will also look for log_post
-  stopifnot(all(params %in% names(post)))
-  stopifnot(all(paste0(params, "start") %in% names(post)))
-  stopifnot(all(paste0(params, "stop") %in% names(post)))
-  stopifnot("log_post" %in% names(post))
+  stopifnot(all(params %in% names(joint_post)))
+  stopifnot(all(paste0(params, "start") %in% names(joint_post)))
+  stopifnot(all(paste0(params, "stop") %in% names(joint_post)))
+  stopifnot("log_post" %in% names(joint_post))
 
-  probs <- exp(post$log_post)
-  bucket <- sample(seq(nrow(post)), size=nsamps, prob = probs, replace=T)
+  probs <- exp(joint_post$log_post)
+  bucket <- sample(seq(nrow(joint_post)), size=nsamps, prob = probs, replace=T)
 
   samps <- sapply(seq_along(params), FUN=function(i){
     offset <- runif(nsamps, 0, 1)
     st <- paste0(params[i], "start")
     sp <- paste0(params[i], "stop")
-    width <- post[, sp]-post[,st]
-    post[bucket, st] + offset*width[bucket]
+    width <- joint_post[, sp]-joint_post[,st]
+    joint_post[bucket, st] + offset*width[bucket]
   })
   samps <- data.frame(samps)
   names(samps) <- params
