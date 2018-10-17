bfc4398992078d848322f233c21af1904bfc9fcc
Jean Morrison <jeanm@uchicago.edu>
2018-10-16 22:27:38 -0500

    remove mixsqp dependency

--- R/package.R
+++ R/package.R
@@ -5,7 +5,7 @@
 #' @docType package
 #' @author Jean Morrison <jeanm@uchicago.edu>
 #' @import Rcpp ashr dplyr ggplot2 gridExtra loo matrixStats parallel
-#' @import purrr tidyr intervals mixsqp numDeriv
+#' @import purrr tidyr intervals numDeriv
 #' @importFrom Rcpp sourceCpp
 #' @importFrom RcppParallel RcppParallelLibs
 #' @useDynLib cause
