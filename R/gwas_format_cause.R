43f51aab08f3828249a63bbdba31d5aa7b0e5b29
Jean Morrison <jeanm@uchicago.edu>
2018-10-16 22:24:02 -0500

    Add allele flipping code

--- R/gwas_format_cause.R
+++ R/gwas_format_cause.R
@@ -4,38 +4,146 @@
 #'@param X2 data.frame with data for GWAS 2
 #'@param snp_name_cols A vector of length 2 specifying the name of the snp column
 #'in X1 and X2 respectively. This is the column on which the data will be merged.
-#'#'@param beta_hat_col A vector of length 2 specifying the name of the snp column
+#'@param beta_hat_col A vector of length 2 specifying the name of the snp column
 #'in X1 and X2 respectively. If effect sizes are provided as odds ratios they must be converted
 #'back into coeffecient estimates by taking the log.
-#'#'@param se_cols A vector of length 2 specifying the name of the snp column
+#'@param se_cols A vector of length 2 specifying the name of the snp column
 #'in X1 and X2 respectively.
+#'@param A1_cols Column names for effect allele
+#'@param A2_cols Column names for other allele
 #'@return An object of class cause_data
 #'@export
 gwas_format_cause <- function(X1, X2, snp_name_cols=c("snp", "snp"),
                               beta_hat_cols = c("beta_hat", "beta_hat"),
-                              se_cols = c("se", "se")){
+                              se_cols = c("se", "se"),
+                              A1_cols= c("A1", "A1"),
+                              A2_cols = c("A2", "A2")){
 
   X1 <- X1 %>% rename(snp = snp_name_cols[1],
                       beta_hat_1 = beta_hat_cols[1],
-                      seb1 = se_cols[1]) %>%
-        select(snp, beta_hat_1, seb1)
+                      seb1 = se_cols[1],
+                      A1 = A1_cols[1],
+                      A2 = A2_cols[1]) %>%
+        select(snp, beta_hat_1, seb1, A1, A2)
 
   X2 <- X2 %>% rename(snp = snp_name_cols[2],
                       beta_hat_2 = beta_hat_cols[2],
-                      seb2 = se_cols[2]) %>%
-        select(snp, beta_hat_2, seb2)
+                      seb2 = se_cols[2],
+                      A1 = A1_cols[2],
+                      A2 = A2_cols[2]) %>%
+        select(snp, beta_hat_2, seb2, A1, A2)
   cat("There are ", nrow(X1), " variants in GWAS 1 and ", nrow(X2),
       " variants in GWAS2.\n")
+  #Check case of allele coding
+  upper1 <- X1$A1[1] == toupper(X1$A1[1])
+  upper2 <- X2$A1[1] == toupper(X2$A1[1])
+
+  if(upper1){
+    if(!all(X1$A1 %in% c("A", "C", "T", "G"))){
+      stop("Allele 1 in data set 1 contains illegal characters.")
+    }
+    if(!all(X1$A2 %in% c("A", "C", "T", "G"))){
+      stop("Allele 2 in data set 1 contains illegal characters.")
+    }
+  }else{
+    if(!all(X1$A1 %in% c("a", "c", "t", "g"))){
+      stop("Allele 1 in data set 1 contains illegal characters.")
+    }
+    if(!all(X1$A2 %in% c("a", "c", "t", "g"))){
+      stop("Allele 2 in data set 1 contains illegal characters.")
+    }
+  }
+  if(upper2){
+    if(!all(X2$A1 %in% c("A", "C", "T", "G"))){
+      stop("Allele 1 in data set 2 contains illegal characters.")
+    }
+    if(!all(X2$A2 %in% c("A", "C", "T", "G"))){
+      stop("Allele 2 in data set 2 contains illegal characters.")
+    }
+  }else{
+    if(!all(X2$A1 %in% c("a", "c", "t", "g"))){
+      stop("Allele 1 in data set 2 contains illegal characters.")
+    }
+    if(!all(X2$A2 %in% c("a", "c", "t", "g"))){
+      stop("Allele 2 in data set 2 contains illegal characters.")
+    }
+  }
+
+  X1 <- remove_ambiguous(X1, upper = upper1)
+  X2 <- remove_ambiguous(X2, upper = upper2)
+
+  X1 <- align_beta(X1, "beta_hat_1", upper1)
+  X2 <- align_beta(X2, "beta_hat_2", upper2)
+
+  cat("After removing ambiguous SNPs, there are ",
+      nrow(X1), " variants in GWAS 1 and ", nrow(X2),
+      " variants in GWAS2.\n")
+
+
   X <-  inner_join(X1, X2, by="snp") %>%
         filter(!is.na(seb1) & !is.na(seb2) &
                  !is.na(beta_hat_1) & !is.na(beta_hat_2)) %>%
         filter(is.finite(seb1) & is.finite(seb2) &
                  is.finite(beta_hat_1) & is.finite(beta_hat_2)) %>%
         filter(seb1 >= 0 & seb2 >= 0)
-
-  cat("There are ", nrow(X), " variants overlapping which can be used with CAUSE.")
+  if(!upper1){
+    X  <- X %>% mutate(A2.x = toupper(A2.x))
+  }
+  if(!upper2){
+    X  <- X %>% mutate(A2.y = toupper(A2.y))
+  }
+  X <- filter(X, toupper(A2.x) == toupper(A2.y)) %>%
+       select(-A1.y, -A2.y) %>%
+       rename(A1 = A1.x, A2 = A2.x) %>%
+      select(snp, beta_hat_1, seb1, beta_hat_2, seb2, A1, A2)
+  cat("After merging and removing variants with inconsistent alleles, ",
+      "there are ", nrow(X), " variants overlapping which can be used with CAUSE.")
   new_cause_data(X)
 
+}
+
+remove_ambiguous <- function(X, upper=TRUE){
+  if(upper){
+    X <- X %>% dplyr::filter(!(A1 == "G" & A2 == "C") &
+                             !(A1 == "C" & A2 == "G") &
+                             !(A1 == "A" & A2 == "T") &
+                             !(A1 == "T" & A2 == "A"))
+    return(X)
+  }
+  X <- X %>% filter(!(A1 == "g" & A2 == "c") &
+                             !(A1 == "c" & A2 == "g") &
+                             !(A1 == "a" & A2 == "t") &
+                             !(A1 == "t" & A2 == "a"))
+  return(X)
+}
+
+#Flip signs and strabds so that allele 1 is allways A
+align_beta <- function(X, beta_hat_name, upper=TRUE){
+  flp = c("A" = "T", "G" = "C", "T" = "A",
+          "C" = "G", "a"  = "t", "t" = "a",
+          "c" = "g", "g" = "c")
+  if(upper){
+   X <- X %>% mutate( flip_strand = A1 == "T" | A2 == "T")
+  }else{
+   X <- X %>% mutate( flip_strand = A1 == "t" | A2 == "t")
+  }
+  X <- X %>% mutate(A1flp = case_when(flip_strand ~ flp[A1],
+                                      TRUE ~ A1),
+                    A2flp = case_when(flip_strand ~ flp[A2],
+                                      TRUE ~ A2),
+                    temp = case_when(A1flp == "A" | A1flp == "a" ~ get(beta_hat_name),
+                                                   TRUE ~ -1*get(beta_hat_name))) %>%
+            select(-A1, -A2) %>%
+            mutate(A1 = case_when(A1flp == "A" | A1flp=="a" ~ A1flp,
+                                   TRUE ~ A2flp),
+                    A2 = case_when(A1flp == "A" | A1flp=="a" ~ A2flp,
+                                   TRUE ~ A1flp)) %>%
+            select(-A1flp, -A2flp, -flip_strand)
+  ix <- which(names(X)==beta_hat_name)
+  X <- X[,-ix]
+  ix <- which(names(X) == "temp")
+  names(X)[ix] <- beta_hat_name
+  return(X)
 }
 
 new_cause_data <- function(x = data.frame()){
