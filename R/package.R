ea577e412ef1ba7589bff652d102fd6fb5ae2df0
Jean Morrison <jeanm@uchicago.edu>
2018-10-22 17:02:55 -0500

    started updating imports

--- R/package.R
+++ R/package.R
@@ -4,8 +4,9 @@
 #'
 #' @docType package
 #' @author Jean Morrison <jeanm@uchicago.edu>
-#' @import Rcpp ashr dplyr ggplot2 gridExtra loo matrixStats parallel
+#' @import Rcpp ashr dplyr ggplot2 gridExtra matrixStats parallel
 #' @import purrr tidyr intervals numDeriv
+#' @importFrom loo loo compare
 #' @importFrom Rcpp sourceCpp
 #' @importFrom RcppParallel RcppParallelLibs
 #' @useDynLib cause
