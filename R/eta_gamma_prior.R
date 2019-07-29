593182e35e0cb4aabfbf4390547d3df0511a9535
Jean Morrison <jeanm@uchicago.edu>
2019-07-29 13:54:43 -0500

    Add a p-value threshold to eta_gamma_prior

--- R/eta_gamma_prior.R
+++ R/eta_gamma_prior.R
@@ -5,15 +5,18 @@
 #'@param X An object of class cause_data containing data for the two traits.
 #'@param variants A vector of variants to include in the analysis.
 #'@param prob See Details
+#'@param pval_thresh A p-value threshold applied to trait 1 (M). This prevents
+# inclusion of variants with beta_hat_1 very close to 0.
 #'@details The function will return a variance, sigma, such that
 #'P(abs(z) > k) = prob where z is a N(0, sigma) random variable, k = max(abs(beta_hat_2/beta_hat_1))
 #'and prob is specified in the arguments of the function.
 #'@return A prior variance for gamma and eta (scalar)
-eta_gamma_prior <- function(X, variants, prob = 0.05){
+eta_gamma_prior <- function(X, variants, prob = 0.05, pval_thresh = 1e-3){
   stopifnot(inherits(X, "cause_data"))
   if(! missing(variants)){
     X <- filter(X, snp %in% variants)
   }
+  X <- X %>% filter(2*pnorm(-abs(beta_hat_1/seb1)) < pval_thresh)
   z <- with(X, max(abs(beta_hat_2/beta_hat_1), na.rm=TRUE))
   f <- function(sd){
     abs(prob/2 - pnorm(-z, sd=sd))
