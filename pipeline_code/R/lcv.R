060cfa7da65cfd6136493faab3f2c46a7833b5d4
Jean Morrison <jeanm@uchicago.edu>
2019-10-17 19:05:05 -0500

    make ld score location not hardcoded

--- pipeline_code/R/lcv.R
+++ pipeline_code/R/lcv.R
@@ -6,6 +6,7 @@ library(cause)
 args <- commandArgs(trailingOnly=TRUE)
 file1 <- args[1]
 file2 <- args[2]
+ld_score_dir <- args[3]
 output_file <- args[3]
 
 X1 <- read_standard_format(file1)
@@ -14,18 +15,18 @@ X2 <- read_standard_format(file2)
 X <- gwas_merge(X1, X2, X1_formatted=TRUE, X2_formatted = TRUE)
 X$p_value <- with(X1, p_value[match(X$snp, snp)])
 
-#hm3_snps <- read_tsv("/project2/xinhe/ldsc_reference_files/eur_w_ld_chr/w_hm3.snplist")
 
 X_ldsc <- purrr::map_df(1:22, function(i){
     cat(i, " ")
-    ldscores <- read_tsv(paste0("/project2/xinhe/ldsc_reference_files/eur_w_ld_chr/", i, ".l2.ldscore.gz")) %>%
+    #ldscores <- read_tsv(paste0("/project2/xinhe/ldsc_reference_files/eur_w_ld_chr/", i, ".l2.ldscore.gz")) %>%
+    ldscores <- read_tsv(paste0(ld_score_dir, i, ".l2.ldscore.gz")) %>%
                 select(SNP, L2, CHR, BP) %>%
                 rename(snp = SNP)
     inner_join(X, ldscores, by="snp")
 })
 o <- with(X_ldsc, order(CHR, BP))
 X_ldsc <- X_ldsc[o,]
-lcv_res <- with(X_ldsc, 
+lcv_res <- with(X_ldsc,
                 causeSims::RunLCV(L2,
                        beta_hat_1/seb1,beta_hat_2/seb2,
                        no.blocks=100,crosstrait.intercept=1,ldsc.intercept=1,
