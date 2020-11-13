854e5462de6bfe25de238bae1f6bd9b4c993b261
Jean Morrison <jeanm@uchicago.edu>
2020-11-13 12:57:59 -0500

    filter merged data for A1 == A

--- pipeline_code/R/merge_data.R
+++ pipeline_code/R/merge_data.R
@@ -6,8 +6,10 @@ file1 <- args[1]
 file2 <- args[2]
 output_file <- args[3]
 
-X1 <- read_standard_format(file1)
-X2 <- read_standard_format(file2)
+X1 <- read_standard_format(file1) %>%
+      filter(A1 == "A")
+X2 <- read_standard_format(file2) %>%
+      filter(A1=="A")
 
 X <- gwas_merge(X1, X2, X1_formatted=TRUE, X2_formatted = TRUE)
 X$p_value <- with(X1, p_value[match(X$snp, snp)])
